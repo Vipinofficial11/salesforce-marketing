3e4e14a0aa436fb40fd516b7e5f84d3557c1f648
Vipinofficial11 <vipin.bhatt@cloudsufi.com>
2023-03-26 17:14:01 +0530

    Secret manager access changed

--- src/e2e-test/java/io/cdap/plugin/sfmcsource/actions/SfmcSourcePropertiesPageActions.java
+++ src/e2e-test/java/io/cdap/plugin/sfmcsource/actions/SfmcSourcePropertiesPageActions.java
@@ -136,7 +136,7 @@ public class SfmcSourcePropertiesPageActions {
         expectedOutput.add(line);
       }
     }
-    // Testing this branch on github Actions. Trigger.
+    // Testing this branch on github Actions. Trigger
     List<String> bigQueryDatasetTables = new ArrayList<>();
     TableResult tablesSchema = getTableNamesFromDataSet(dataset);
     tablesSchema.iterateAll().forEach(value -> bigQueryDatasetTables.add(value.get(0).getValue().toString()));
