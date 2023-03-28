43ec63e5e5e717db950945bb7e2b70dd2ea4d845
Vipinofficial11 <vipin.bhatt@cloudsufi.com>
2023-03-28 20:08:26 +0530

    fix maven issue

--- src/e2e-test/java/io/cdap/plugin/sfmcsource/actions/SfmcSourcePropertiesPageActions.java
+++ src/e2e-test/java/io/cdap/plugin/sfmcsource/actions/SfmcSourcePropertiesPageActions.java
@@ -136,7 +136,7 @@ public class SfmcSourcePropertiesPageActions {
         expectedOutput.add(line);
       }
     }
-    // Testing this branch on github Actions. fix maven issue
+    // Testing this branch on github Actions. running chrome
     List<String> bigQueryDatasetTables = new ArrayList<>();
     TableResult tablesSchema = getTableNamesFromDataSet(dataset);
     tablesSchema.iterateAll().forEach(value -> bigQueryDatasetTables.add(value.get(0).getValue().toString()));
